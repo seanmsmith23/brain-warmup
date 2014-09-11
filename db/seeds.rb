291ae2fd060efc60dc027efde36f3650fa29d85a
Sean Smith <seansmith1020@gmail.com>
2014-09-11 09:36:15 -0600

    autmatically generate seeds from seed file

---
+++ db/seeds.rb
@@ -0,0 +1,56 @@
+Brain.create!(animal: 'Mountain beaver', body: 1.35, brain: 8.1)
+
+Brain.create!(animal: 'Cow', body: 465, brain: 423)
+
+Brain.create!(animal: 'Grey wolf', body: 36.33, brain: 119.5)
+
+Brain.create!(animal: 'Goat', body: 27.66, brain: 115)
+
+Brain.create!(animal: 'Guinea pig', body: 1.04, brain: 5.5)
+
+Brain.create!(animal: 'Dipliodocus', body: 11700, brain: 50)
+
+Brain.create!(animal: 'Asian elephant', body: 2547, brain: 4603)
+
+Brain.create!(animal: 'Donkey', body: 187.1, brain: 419)
+
+Brain.create!(animal: 'Horse', body: 521, brain: 655)
+
+Brain.create!(animal: 'Potar monkey', body: 10, brain: 115)
+
+Brain.create!(animal: 'Cat', body: 3.3, brain: 25.6)
+
+Brain.create!(animal: 'Giraffe', body: 529, brain: 680)
+
+Brain.create!(animal: 'Gorilla', body: 207, brain: 406)
+
+Brain.create!(animal: 'Human', body: 62, brain: 1320)
+
+Brain.create!(animal: 'African elephant', body: 6654, brain: 5712)
+
+Brain.create!(animal: 'Triceratops', body: 9400, brain: 70)
+
+Brain.create!(animal: 'Rhesus monkey', body: 6.8, brain: 179)
+
+Brain.create!(animal: 'Kangaroo', body: 35, brain: 56)
+
+Brain.create!(animal: 'Golden hamster', body: 0.12, brain: 1)
+
+Brain.create!(animal: 'Mouse', body: 0.023, brain: 0.4)
+
+Brain.create!(animal: 'Rabbit', body: 2.5, brain: 12.1)
+
+Brain.create!(animal: 'Sheep', body: 55.5, brain: 175)
+
+Brain.create!(animal: 'Jaguar', body: 100, brain: 157)
+
+Brain.create!(animal: 'Chimpanzee', body: 52.16, brain: 440)
+
+Brain.create!(animal: 'Rat', body: 0.28, brain: 1.9)
+
+Brain.create!(animal: 'Brachiosaurus', body: 87000, brain: 154.5)
+
+Brain.create!(animal: 'Mole', body: 0.122, brain: 3)
+
+Brain.create!(animal: 'Pig', body: 192, brain: 180)
+
